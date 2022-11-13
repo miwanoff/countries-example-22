817ffa8dd09fe2bc66eb83c3b8b34b20863f8316
miwanoff <marina.iva.noffa@gmail.com>
2022-11-13 15:04:43 +0200

    Initial commit

---
+++ db.php
@@ -0,0 +1,35 @@
+<?php
+$countries = array(
+    array(
+        "name" => "Англия",
+        "capital" => "Лондон",
+        "area" => 130395,
+        "population" => array(
+            "2000" => 58800000,
+            "2010" => 63200000,
+        ),
+    ),
+    array(
+        "name" => "Франция",
+        "capital" => "Париж",
+        "area" => 640679,
+        "population" => array(
+            "2000" => 59258000,
+            "2010" => 59278000,
+        ),
+    ),
+    array(
+        "name" => "Германия",
+        "capital" => "Берлин",
+        "area" => 357021,
+        "population" => array(
+            "2000" => 82260000,
+            "2010" => 81752000,
+        ),
+    ),
+);
+
+$users = array(
+    "user" => "user",
+    "admin" => "admin",
+);
