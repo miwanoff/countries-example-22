642168ca3bc55bb3f0b8e53091849cdaaef9673c
miwanoff <marina.iva.noffa@gmail.com>
2022-11-13 15:11:14 +0200

    Add header

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Страны</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="js/scripts.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1>Страны</h1>
+    </header>
+    <hr />
