e3a239cc2f0e05444694d143014df8c28f0da6b5
miwanoff <marina.iva.noffa@gmail.com>
2022-11-13 18:14:50 +0200

    Fix translation

--- hello.php
+++ hello.php
@@ -2,12 +2,12 @@
 ob_start();
 include "header.php";
 include "action.php";
-echo "\n<h2>Секретная информация</h2>\n";
+echo "\n<h2>Secret information</h2>\n";
 if (isset($_GET['login']) && $_GET['login'] != "") {
     $admin = $_GET['login'];
     if (check_log("admin") == true) {
-        echo "<p>Привет, $admin</p>";
-        echo "<p>Сводка погоды</p>";
+        echo "<p>Hello, $admin</p>";
+        echo "<p>Weather</p>";
     }
 } else {
     header("Location: index.php");
