e2bd17b9115593cdf0f2475ad2ce4fcb5aaec59c
miwanoff <marina.iva.noffa@gmail.com>
2022-11-13 17:26:53 +0200

    Fix copyright

--- footer.php
+++ footer.php
@@ -7,6 +7,9 @@
         <div class="row">
             <div class="col-lg-12">
                 <p>Copyright © 2022 <a href="#">Kit</a> Company. All rights reserved.
+                    <br>Design: <a href="https://templatemo.com" target="_blank"
+                        title="free CSS templates">TemplateMo</a>
+                </p>
                 </p>
             </div>
         </div>
